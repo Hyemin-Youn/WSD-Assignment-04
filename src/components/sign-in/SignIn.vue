3de0ade0f1dde0427a299bdcaf1c1b0aba319089
Hyemin-Youn <hyemin9973@gmail.com>
2024-12-21 14:19:24 +0900

    update

--- src/components/sign-in/SignIn.vue
+++ src/components/sign-in/SignIn.vue
@@ -152,7 +152,7 @@ export default {
     },
     handleKakaoLogin() {
       const clientId = process.env.VUE_APP_KAKAO_API_KEY;
-      const redirectUri = "YOUR_REDIRECT_URI";
+      const redirectUri = "https://hyemin-youn.github.io/WSD-Assignment-04/#/signin";
       const kakaoAuthUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code`;
       window.location.href = kakaoAuthUrl;
     },
