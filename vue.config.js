2bbafb978810366f1ede5e6aa576e1f7902d81f8
Hyemin-Youn <hyemin9973@gmail.com>
2024-12-21 22:20:11 +0900

    update publicpath

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production' 
-    ? '/WSD-Assignment-04/#/' // GitHub Pages 레포지토리 이름
+    ? '/WSD-Assignment-04/' // GitHub Pages 레포지토리 이름
     : '/',
   transpileDependencies: true,
   lintOnSave: false, // 저장 시 ESLint 비활성화
