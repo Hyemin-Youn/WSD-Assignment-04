e12c8bc7cb0940d48bf653662e85a87d1479affa
Hyemin-Youn <hyemin9973@gmail.com>
2024-12-22 23:13:14 +0900

    update

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production' 
-    ? '/' // GitHub Pages 레포지토리 이름
+    ? '/WSD-Assignment-04/' // GitHub Pages 레포지토리 이름
     : '/',
     devServer: {
       historyApiFallback: true, // History 모드에서 개발 서버 지원
