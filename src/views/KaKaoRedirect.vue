f259ac83104020d2b3ad1fc364f5366864778f9b
Hyemin-Youn <hyemin9973@gmail.com>
2024-12-22 21:18:46 +0900

    update

--- src/views/KaKaoRedirect.vue
+++ src/views/KaKaoRedirect.vue
@@ -1,53 +1,46 @@
 <template>
-    <div>
-      <p>Redirecting... Please wait.</p>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    mounted() {
-      const query = new URLSearchParams(window.location.search);
-      const code = query.get("code");
-  
-      if (code) {
-        this.exchangeAuthorizationCode(code);
-      } else {
-        console.error("No authorization code found");
-        this.$router.push("/signin"); // 코드가 없을 경우 로그인 페이지로 이동
-      }
-    },
-    methods: {
-      exchangeAuthorizationCode(code) {
-        fetch("https://kauth.kakao.com/oauth/token", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-          },
-          body: new URLSearchParams({
-            grant_type: "authorization_code",
-            client_id: process.env.VUE_APP_KAKAO_API_KEY,
-            redirect_uri: "https://hyemin-youn.github.io/WSD-Assignment-04/oauth/callback",
-            code: code,
-          }),
+  <div>
+    <p>로그인 처리 중...</p>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    const query = new URLSearchParams(window.location.search);
+    const code = query.get("code");
+
+    if (code) {
+      this.exchangeKakaoToken(code);
+    }
+  },
+  methods: {
+    exchangeKakaoToken(code) {
+      fetch("https://kauth.kakao.com/oauth/token", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: new URLSearchParams({
+          grant_type: "authorization_code",
+          client_id: process.env.VUE_APP_KAKAO_API_KEY,
+          redirect_uri: "https://hyemin-youn.github.io/WSD-Assignment-04/kakao-redirect",
+          code: code,
+        }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.access_token) {
+            localStorage.setItem("kakaoToken", data.access_token);
+            this.$router.push("/home"); // 홈으로 리다이렉트
+          } else {
+            console.error("Failed to get Access Token:", data);
+          }
         })
-          .then((response) => response.json())
-          .then((data) => {
-            if (data.access_token) {
-              console.log("Access Token:", data.access_token);
-              localStorage.setItem("kakaoToken", data.access_token);
-              this.$router.push("/home"); // 성공 시 홈으로 리디렉션
-            } else {
-              console.error("Failed to get Access Token", data);
-              this.$router.push("/signin"); // 실패 시 로그인 페이지로 리디렉션
-            }
-          })
-          .catch((error) => {
-            console.error("Error during token exchange", error);
-            this.$router.push("/signin");
-          });
-      },
+        .catch((error) => {
+          console.error("Error during token exchange:", error);
+        });
     },
-  };
-  </script>
-  
+  },
+};
+</script>
