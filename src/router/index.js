fd85f3ac2c1e01947a0472bab2e399396b911f8b
Hyemin-Youn <hyemin9973@gmail.com>
2024-12-22 17:16:47 +0900

    update

--- src/router/index.js
+++ src/router/index.js
@@ -9,7 +9,6 @@ import Search from "@/views/Search.vue";
 import PopularInfinite from "@/views/PopularInfinite.vue";
 import PopularTable from "@/views/PopularTable.vue";
 import Popular from "@/components/Popular.vue";
-import KakaoRedirect from "@/components/KakaoRedirect.vue";
 
 
 const routes = [
