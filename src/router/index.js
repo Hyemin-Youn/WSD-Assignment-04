8ece37e1af2a16b9500dd8dbf4613c48e7cd7bfb
Hyemin-Youn <hyemin9973@gmail.com>
2024-12-22 23:07:11 +0900

    update router

--- src/router/index.js
+++ src/router/index.js
@@ -8,39 +8,39 @@ import SliderContent from '../components/SliderContent.vue';
 import Search from "@/views/Search.vue";
 import PopularInfinite from "@/views/PopularInfinite.vue";
 import PopularTable from "@/views/PopularTable.vue";
-import Popular from "@/components/Popular.vue"; // 경로 수정
-import KakaoRedirect from "@/components/KakaoRedirect.vue"; // 경로 수정
+import Popular from "@/components/Popular.vue";
+
 
 const routes = [
-  {
-    path: '/signin',
-    name: 'SignIn',
+  { 
+    path: '/signin', 
+    name: 'SignIn', 
     component: SignIn,
     meta: { hideNavbar: true }, // Navbar를 숨기기 위한 메타 데이터
   },
-  {
-    path: '/home',
-    name: 'Home',
+  { 
+    path: '/home', 
+    name: 'Home', 
     component: Home,
     meta: { requiresAuth: true },
   },
-  {
-    path: '/',
-    redirect: { name: 'SignIn' }, // 기본 경로 리디렉션
-  },
+  { 
+    path: '/', 
+    redirect: '/signin',
+  },  
   {
     path: "/movies/:id",
-    component: MovieDetail,
+    component: MovieDetail 
   },
   {
     path: '/wishlist',
     name: 'Wishlist',
-    component: Wishlist,
+    component: Wishlist 
   },
   {
     path: "/slider",
     name: 'SliderContent',
-    component: SliderContent,
+    component: SliderContent, 
   },
   {
     path: "/popular",
@@ -56,21 +56,16 @@ const routes = [
         component: PopularInfinite,
       },
     ],
-  },
+  },  
   {
     path: "/search",
     name: "Search",
     component: Search,
   },
-  {
-    path: "/kakao-redirect",
-    name: "KakaoRedirect",
-    component: KakaoRedirect,
-  },
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(), // Hash 모드 사용
+  history: createWebHashHistory(),
   routes,
 });
 
@@ -78,9 +73,10 @@ router.beforeEach((to, from, next) => {
   const isAuthenticated = store.getters.isAuthenticated;
 
   if (to.name === 'SignIn' && isAuthenticated) {
-    return next('/home'); // 인증된 사용자가 로그인 페이지로 접근할 때
+    next('/home'); // 인증된 사용자가 로그인 페이지로 접근할 때
+  } else {
+    next(); // 나머지 경우는 통과
   }
-  next(); // 나머지 경우는 통과
 });
 
 export default router;
