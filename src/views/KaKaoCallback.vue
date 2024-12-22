bbd1d8cb33ca7fd46cc60c0e084178c20bc68180
Hyemin-Youn <hyemin9973@gmail.com>
2024-12-23 00:03:10 +0900

    KaKaoCallback.vue

--- src/views/KaKaoCallback.vue
+++ src/views/KaKaoCallback.vue
@@ -1,22 +1,21 @@
 <template>
-    <div>Redirecting...</div>
-  </template>
-  
-  <script>
-  export default {
-    mounted() {
-      const query = new URLSearchParams(window.location.search);
-      const code = query.get('code');
-  
-      if (code) {
-        console.log("Authorization code received:", code);
-        // Hash 모드로 다시 리다이렉션
-        const redirectUrl = `/#/signin?code=${code}`;
-        window.location.href = redirectUrl;
-      } else {
-        console.error("Authorization Code가 없습니다.");
-      }
-    },
-  };
-  </script>
-  
+  <div>Redirecting...</div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    const query = new URLSearchParams(window.location.search);
+    const code = query.get("code");
+
+    if (code) {
+      console.log("Authorization code received:", code);
+      // Hash 모드로 /home으로 리다이렉트
+      const redirectUrl = `/#/home?code=${code}`;
+      window.location.href = redirectUrl;
+    } else {
+      console.error("Authorization Code가 없습니다.");
+    }
+  },
+};
+</script>
