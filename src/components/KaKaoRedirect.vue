0118b7f6e86ede48c83b4cd5f3df1bfc5980fee5
Hyemin-Youn <hyemin9973@gmail.com>
2024-12-22 22:23:25 +0900

    update

--- src/components/KaKaoRedirect.vue
+++ src/components/KaKaoRedirect.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <p>로그인 처리 중...</p>
+    <p>카카오 로그인 처리 중...</p>
   </div>
 </template>
 
@@ -11,36 +11,8 @@ export default {
     const code = query.get("code");
 
     if (code) {
-      this.exchangeKakaoToken(code);
+      this.$emit("handleKakaoCallback", code);
     }
-  },
-  methods: {
-    exchangeKakaoToken(code) {
-      fetch("https://kauth.kakao.com/oauth/token", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-        body: new URLSearchParams({
-          grant_type: "authorization_code",
-          client_id: process.env.VUE_APP_KAKAO_API_KEY,
-          redirect_uri: "https://hyemin-youn.github.io/WSD-Assignment-04/kakao-redirect",
-          code: code,
-        }),
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          if (data.access_token) {
-            localStorage.setItem("kakaoToken", data.access_token);
-            this.$router.push("/home"); // 홈으로 리다이렉트
-          } else {
-            console.error("Failed to get Access Token:", data);
-          }
-        })
-        .catch((error) => {
-          console.error("Error during token exchange:", error);
-        });
-    },
   },
 };
 </script>
