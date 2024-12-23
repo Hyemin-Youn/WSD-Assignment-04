1fabdffe0e35d43b2861981d9f0e468dbf3cea12
Hyemin-Youn <hyemin9973@gmail.com>
2024-12-23 16:55:03 +0900

    update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -5,10 +5,6 @@ module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production' 
     ? '/WSD-Assignment-04/' // GitHub Pages 레포지토리 이름
     : '/',
-  devServer: {
-    historyApiFallback: true, // `hash` 모드에서는 불필요
-    allowedHosts: 'all', // 필요시 개발 서버가 모든 네트워크에서 접근 가능하도록 설정
-  },
   transpileDependencies: true,
   lintOnSave: process.env.NODE_ENV !== 'production', // 배포 시 Lint 비활성화, 개발 시 활성화
   configureWebpack: {
