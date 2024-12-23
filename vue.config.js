d9cdb285c1d2109e2bfc314cbc93c1d78cac99e7
Hyemin-Youn <hyemin9973@gmail.com>
2024-12-23 23:01:20 +0900

    update all

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,17 @@
-const path = require("path");
+const { defineConfig } = require('@vue/cli-service');
+const path = require('path');
 
-module.exports = {
+module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production' 
+    ? '/WSD-Assignment-04/' // GitHub Pages 레포지토리 이름
+    : '/',
+  transpileDependencies: true,
+  lintOnSave: process.env.NODE_ENV !== 'production', // 배포 시 Lint 비활성화, 개발 시 활성화
   configureWebpack: {
     resolve: {
       alias: {
-        "@config": path.resolve(__dirname, "src/config"),
+        '@': path.resolve(__dirname, 'src'),
       },
     },
   },
-};
+});
