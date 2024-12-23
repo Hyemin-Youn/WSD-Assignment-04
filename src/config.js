cd3b1e8b5a1999ffac8e5e65fc4f8ea674a16f8f
Hyemin-Youn <hyemin9973@gmail.com>
2024-12-23 22:57:00 +0900

    update all

---
+++ src/config.js
@@ -0,0 +1,7 @@
+export const API_KEY = process.env.VUE_APP_TMDB_API_KEY; // TMDB API Key
+export const BASE_URL = process.env.VUE_APP_BASE_URL; // TMDB Base URL
+
+
+// Kakao Redirect URI
+
+export const KAKAO_JAVASCRIPT_KEY = process.env.VUE_APP_KAKAO_JAVASCRIPT_KEY;
