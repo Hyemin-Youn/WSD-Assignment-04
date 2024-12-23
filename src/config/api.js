90accc7bbaa91dc19daf1e922025c4ae8b97f3d7
Hyemin-Youn <hyemin9973@gmail.com>
2024-12-23 22:48:18 +0900

    update config/api.js

---
+++ src/config/api.js
@@ -0,0 +1,4 @@
+// src/config/api.js
+export const TMDB_API_KEY = "75b7c972001662bd9d37622e0e222947";
+export const BASE_URL = "https://api.themoviedb.org/3";
+export const KAKAO_JAVASCRIPT_KEY = "92d19d65367f8ae3be01c23a17c88014"
