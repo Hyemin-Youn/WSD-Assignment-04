257079c3facdd2f5aa98065d43736644e331d7c4
Hyemin-Youn <hyemin9973@gmail.com>
2024-12-23 22:40:42 +0900

    update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,17 @@
 const { defineConfig } = require('@vue/cli-service');
 const path = require('path');
 
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/WSD-Assignment-04/'
+module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production' 
+    ? '/WSD-Assignment-04/' // GitHub Pages 레포지토리 이름
     : '/',
-  env: {
-    VUE_APP_KAKAO_JAVASCRIPT_KEY: process.env.VUE_APP_KAKAO_JAVASCRIPT_KEY,
+  transpileDependencies: true,
+  lintOnSave: process.env.NODE_ENV !== 'production', // 배포 시 Lint 비활성화, 개발 시 활성화
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
+    },
   },
-};
-
+});
