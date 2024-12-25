3728ebce26de6d87d532f582b26058d38c7bba1f
Hyemin-Youn <hyemin9973@gmail.com>
2024-12-26 03:43:23 +0900

    update origin al header & kakaocallback

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,53 +1,53 @@
 <template>
-  <header class="app-header">
-    <div class="logo">My App</div>
-    <div class="user-info" v-if="user">
-      <img :src="user.profile_image" alt="프로필 이미지" />
-      <span>안녕하세요, {{ user.nickname }}님!</span>
-      <button @click="logout">로그아웃</button>
-    </div>
-    <div v-else>
-      <button @click="$router.push('/signin')">로그인</button>
-    </div>
-  </header>
-</template>
-
-<script>
-export default {
-  computed: {
-    user() {
-      return this.$store.state.user; // Vuex에서 사용자 정보 가져오기
+    <header class="app-header">
+      <div class="logo">My App</div>
+      <div class="user-info" v-if="user">
+        <span>안녕하세요, {{ user.nickname }}님!</span>
+        <img :src="user.profile_image" alt="프로필 이미지" />
+        <button @click="logout">로그아웃</button>
+      </div>
+      <div v-else>
+        <button @click="$emit('kakao-login')">로그인</button>
+      </div>
+    </header>
+  </template>
+  
+  <script>
+  export default {
+    computed: {
+      user() {
+        return this.$store.state.user;
+      },
     },
-  },
-  methods: {
-    logout() {
-      this.$store.commit("logout");
-      alert("로그아웃되었습니다.");
-      this.$router.push("/signin");
+    methods: {
+      logout() {
+        this.$store.commit("logout");
+        alert("로그아웃되었습니다.");
+      },
     },
-  },
-};
-</script>
-
-<style scoped>
-.app-header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 10px 20px;
-  background-color: #333;
-  color: white;
-}
-
-.user-info {
-  display: flex;
-  align-items: center;
-}
-
-.user-info img {
-  width: 40px;
-  height: 40px;
-  border-radius: 50%;
-  margin-right: 10px;
-}
-</style>
+  };
+  </script>
+  
+  <style scoped>
+  .app-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px 20px;
+    background-color: #333;
+    color: white;
+  }
+  
+  .user-info {
+    display: flex;
+    align-items: center;
+  }
+  
+  .user-info img {
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    margin-left: 10px;
+  }
+  </style>
+  
