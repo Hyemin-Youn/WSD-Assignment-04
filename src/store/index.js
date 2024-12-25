2d2cb297ca60d7df8205c4874745daabd7ac67fe
Hyemin-Youn <hyemin9973@gmail.com>
2024-12-25 15:55:29 +0900

    update store

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,4 @@
+// store/index.js
 import { createStore } from "vuex";
 
 const store = createStore({
@@ -29,10 +30,8 @@ const store = createStore({
       );
 
       if (existingMovieIndex === -1) {
-        // 영화가 찜 목록에 없으면 추가
         state.wishlist.push(movie);
       } else {
-        // 영화가 찜 목록에 있으면 제거
         state.wishlist.splice(existingMovieIndex, 1);
       }
 
@@ -40,9 +39,9 @@ const store = createStore({
     },
     ADD_SEARCH_HISTORY(state, query) {
       if (!state.recentSearches.includes(query)) {
-        state.recentSearches.unshift(query); // 최신 검색어를 목록 맨 앞에 추가
+        state.recentSearches.unshift(query);
         if (state.recentSearches.length > 10) {
-          state.recentSearches.pop(); // 최근 검색어는 최대 10개까지만 유지
+          state.recentSearches.pop();
         }
         localStorage.setItem(
           "recentSearches",
@@ -51,14 +50,14 @@ const store = createStore({
       }
     },
     DELETE_SEARCH_HISTORY(state, index) {
-      state.recentSearches.splice(index, 1); // 해당 인덱스의 검색어 삭제
+      state.recentSearches.splice(index, 1);
       localStorage.setItem(
         "recentSearches",
         JSON.stringify(state.recentSearches)
       );
     },
     SET_SEARCH_RESULTS(state, results) {
-      state.searchResults = results; // 검색 결과 저장
+      state.searchResults = results;
       localStorage.setItem("searchResults", JSON.stringify(results));
     },
   },
@@ -78,12 +77,12 @@ const store = createStore({
     },
   },
   getters: {
-    wishlist: (state) => state.wishlist, // 찜 목록 가져오기
+    wishlist: (state) => state.wishlist,
     isInWishlist: (state) => (id) => {
-      return state.wishlist.some((movie) => movie.id === id); // 특정 영화가 찜 목록에 있는지 확인
+      return state.wishlist.some((movie) => movie.id === id);
     },
-    recentSearches: (state) => state.recentSearches, // 최근 검색어 가져오기
-    searchResults: (state) => state.searchResults, // 저장된 검색 결과 가져오기
+    recentSearches: (state) => state.recentSearches,
+    searchResults: (state) => state.searchResults,
   },
 });
 
