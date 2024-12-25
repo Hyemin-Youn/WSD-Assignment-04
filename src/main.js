d4a366a73d96f7f2bb7dcea1e46bbd94ea0ee05b
Hyemin-Youn <hyemin9973@gmail.com>
2024-12-26 07:05:48 +0900

    update navbar

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router'; // router 설정 가져오기
 import store from './store';
+import Vuex from "vuex";
 import '@fortawesome/fontawesome-free/css/all.css';
 
 // FontAwesome 설정
@@ -18,6 +19,7 @@ const app = createApp(App);
 // Vue 앱에 플러그인 등록
 app.use(router);
 app.use(store);
+Vue.use(Vuex);
 
 // FontAwesome 컴포넌트 등록
 app.component('FontAwesomeIcon', FontAwesomeIcon);
